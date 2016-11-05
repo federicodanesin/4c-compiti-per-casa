4582035e7d0450c029afb20bcfc65bd4a6ddd58f
federicodanesin <federicodanesin@libero.it>
2016-11-05 12:29:02 +0100

    Add files via upload

---
+++ es 48 pdl.cpp
@@ -0,0 +1,41 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+enum mesi{GEN=1,FEB,MAR,APR,MAG,GIU,LUG,AGO,SET,OTT,NOV,DIC  };
+
+mesi x,y,z;
+
+
+int mese;
+
+int main ()   {
+
+x=GIU;
+y=LUG;
+z=AGO;
+
+
+ cout<<"NUMERO DEI MESI DI VACANZA:"<<x<<'\t'<<y<<'\t'<<z<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+ }
+
+
+
+
+
+
+
+
